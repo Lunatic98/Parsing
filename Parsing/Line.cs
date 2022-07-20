ee800486154feb78e2f5c2ef5937e6e6647c7470
TheEpicBoy <aidarsadreev@mail.ru>
2022-07-20 19:31:55 +0300

    init

---
+++ Parsing/Line.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing
+{
+    public class Line
+    {
+        public string DeclarationNumber;
+        public string CompanyName;
+        public string CompanyInn;
+        public string BuyerName;
+        public string BuyerInn;
+        public DateTime DealDate;
+
+    }
+}
